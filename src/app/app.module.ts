821b54371da0064b1708f0feca47e7211a359ad0
unknown <giovannilamarmora.working@gmail.com>
2022-03-28 15:45:47 +0200

    Added Dashboard accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
